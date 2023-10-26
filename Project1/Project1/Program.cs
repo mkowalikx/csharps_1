12f94019a4aca891d0eaead48d76c699ee5e5f18
Mefjuuuu <1mateuszkowalik@gmail.com>
2023-10-26 02:07:51 +0200

    init

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program {
+
+    static void Maain(String[] args)
+    {
+
+        string data = Console.ReadLine();
+        string[] multiString = data.Split(' ');
+        int age = int.Parse(multiString[1]);
+        int retirementAge = int.Parse(multiString[2]);
+        // int age = int.Parse(Console.ReadLine() ?? string.Empty);
+        // int retirementAge = int.Parse(Console.ReadLine() ?? string.Empty);
+        int result = retirementAge - age;
+        
+        if (age < 0 || retirementAge < 0)
+        {
+            Console.WriteLine("Wiek nie może być ujemny!");
+        }
+        else if (result <= 0)
+        {
+            Console.WriteLine($"Witaj emerycie {multiString[0]}!");
+        }
+        else
+        {
+            if (result == 1)
+            {
+                // ReSharper disable once HeapView.BoxingAllocation
+                Console.WriteLine($"Witaj {multiString[0]}! Do emerytury brakuje Ci {result} rok!");    
+            }
+            else if ((result % 10 == 2 && result != 12) || (result % 10 == 3 && result!=13) || (result % 10 == 4 && result != 14))
+            {
+                // ReSharper disable once HeapView.BoxingAllocation
+                Console.WriteLine($"Witaj {multiString[0]}! Do emerytury brakuje Ci {result} lata!");  
+            }
+            else
+            {
+                // ReSharper disable once HeapView.BoxingAllocation
+                Console.WriteLine($"Witaj {multiString[0]}! Do emerytury brakuje Ci {result} lat!");  
+            }
+        }
+    }
+}
---
+++ Project1/Project1/Program2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Project1 
+{
+    public class Program2 {
+        
+        static void Main(String[] args) {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            string data = Console.ReadLine();
+            // ReSharper disable once HeapView.ObjectAllocation.Evident
+            string[] dataList = data.Split(new[] { ' ', ';' }, StringSplitOptions.RemoveEmptyEntries);
+        
+            // Console.WriteLine(dataList.Length);
+            // Console.WriteLine(dataList[0]);
+            // Console.WriteLine(dataList[1]);
+            // Console.WriteLine(dataList[2]);
+            double a = double.Parse(dataList[0]);
+            double b = double.Parse(dataList[1]);
+            double c = double.Parse(dataList[2]);
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // Console.WriteLine(c);
+            if (a <= 0 || b <= 0 || c <= 0) {
+                Console.WriteLine("Błędne dane. Długości boków muszą być dodatnie!");
+                return;
+            }
+            
+            if (a + b >= c) {
+                double circuit = a + b + c;
+                double p = circuit / 2;
+                double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                //Console.WriteLine($"{Math.Round(circuit, 2)}, {Math.Round(area, 2)}");
+                string type;
+                if ((a * a) + (b * b) == (c * c)) {
+                    type = "prostokątny";
+                }
+                else if (b * b + c * c > a * a && a * a + c * c > b * b && a * a + b * b > c * c) {
+                    type = "ostrokątny";
+                }
+                else {
+                    type = "rozwartokątny";
+                }
+                
+                Console.WriteLine($"obwód = {circuit.ToString("0.00")}");
+                Console.WriteLine($"pole = {area.ToString("0.00")}");            
+                // Console.WriteLine($"obwód = {Math.Round(circuit, 2)}");
+                // Console.WriteLine($"pole = {Math.Round(area, 2)}");
+                Console.WriteLine($"trójkąt jest {type}");
+                if (a == b && b == c && c == a) {
+                    Console.WriteLine("trójkąt równoboczny");
+                }
+                else if (a == b || b == c || c == a) {
+                    Console.WriteLine("trójkąt równoramienny");
+                }
+                
+            }
+            else {
+                Console.WriteLine("Błędne dane. Trójkąta nie można zbudować!");
+            }
+        }
+    }
+}
